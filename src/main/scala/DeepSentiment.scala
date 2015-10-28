0f3b21f420e2dc08b15df091f10c844297f866af
Jason Baldridge <jasonbaldridge@gmail.com>
2015-10-28 16:51:37 -0500

    Removed unnecessary Pair import.

--- src/main/scala/DeepSentiment.scala
+++ src/main/scala/DeepSentiment.scala
@@ -1,6 +1,5 @@
 package expdl4j
 
-import javafx.util.Pair
 import org.deeplearning4j.eval.Evaluation
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer
 import org.deeplearning4j.models.word2vec.Word2Vec
